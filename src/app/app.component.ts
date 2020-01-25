b18cff5f91e6802893e5a98f08845c2321d20a55
samuelJSI92 <isamueljs@outlook.com>
2020-01-25 15:14:00 +0530

    Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+declare var fabric: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public canvasID: string;
+  private canvas: any;
+
+  @Input()
+  set coverURL(URL) {
+    if (URL) {
+      fabric.Image.fromURL(URL, function (oImg) {
+        this.canvas.add(oImg);
+      });
+    }
+  }
+
+  constructor() {
+    this.canvasID = 'conver-preview-canvas-' + new Date().getTime();
+  }
+
+  ngOnInit(): void {
+    this.canvas = new fabric.Canvas(this.canvasID);
+    console.log(this.canvas);
+  }
   title = 'cover-preview';
+
 }
